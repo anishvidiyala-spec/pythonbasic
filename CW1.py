9edad12bd486a52aacfb7536b457032031ccf0f2
anishvidiyala-spec <anish.vidiyala@gmail.com>
2026-05-03 11:56:42 -0400

    Add files via upload

---
+++ CW1.py
@@ -0,0 +1,41 @@
+'''
+Python is a high level programming language.
+1. Easily Readable
+2. Known for its versatality
+
+Rules for naming a variable:
+
+1. It has to always start with a letter or underscore
+2. It can have a combination of letters, number, and underscores
+3. No special symbols
+4. Cannot start with a number
+5. Cannot be a keyword
+'''
+#program 1
+print("Hello, my name is Anish")
+
+#program 3
+print("*** ABOUT ME PAGE ***")
+
+#program 2
+print("Hello, my name is Anish Vidiyala")
+print("I am 12 years old.")
+print("I live in New Hope, Pennsylvania")
+
+#understanding variables and data types
+numbers= 123 #int data type
+decimal = 12.5 #float data type
+name = "Anish" #str data type
+answer = False #bool data type
+print(type(answer))
+
+#program 4
+hobby = "playing piano"
+favorite_sport = "Football"
+print("My favorite sport is" , favorite_sport)
+print("My hobby is" , hobby)
+
+#program 5
+pet = input(str("What pet do you have?"))
+name = input (str("What's your pet's name?"))
+print("I would like to meet", name, pet, "!")
